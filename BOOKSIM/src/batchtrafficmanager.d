41b8155dc7ca65968f476591a55694f61900eecc
Abhishek Kumar Singh <43782906+abhi3121@users.noreply.github.com>
2020-11-25 22:17:51 +0530

    Add files via upload
    booksim config files

---
+++ BOOKSIM/src/batchtrafficmanager.d
@@ -0,0 +1,8 @@
+batchtrafficmanager.o: batchtrafficmanager.cpp packet_reply_info.hpp \
+ flit.hpp booksim.hpp outputset.hpp random_utils.hpp \
+ batchtrafficmanager.hpp config_utils.hpp stats.hpp module.hpp \
+ trafficmanager.hpp networks/network.hpp module.hpp flit.hpp credit.hpp \
+ routers/router.hpp timed_module.hpp module.hpp flitchannel.hpp \
+ channel.hpp globals.hpp timed_module.hpp flit.hpp channel.hpp \
+ config_utils.hpp globals.hpp buffer_state.hpp credit.hpp traffic.hpp \
+ routefunc.hpp injection.hpp
